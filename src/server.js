d3cb8d13e01ecc5bb77621f8c4a6376ade534e21
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-05-05 00:31:17 -0300

    feat: adiciona middleware e rotas de autenticação do usuário

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const authRouter = require('./routes/auth')
+
 const app = express()
 
 app.use(express.json())
 
+app.use('/auth', authRouter)
+
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Servidor iniciado em http://localhost:${PORT}/`))
+app.listen(PORT, () => console.log(`Servidor iniciado em http://localhost:${PORT}`))
